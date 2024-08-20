7f859e16b5c6f283019c6373acc7b367928762e3
Sajeed <syedsajeed6969@gmail.com>
2024-08-20 07:21:40 +0530

    services component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
 
 export const routes: Routes = [
     {
-        path: 'home',
+        path: '',
         component: HomeComponent
     },
     {
-        path: 'abb',
+        path: 'about',
         component: AboutComponent
-    }
+    },
+    {
+        path: 'services',
+        component: ServicesComponent
+    },
 ];
