0b37404b769b08965d2f68f6671cee39c140b3f2
Sajeed <syedsajeed6969@gmail.com>
2024-08-20 08:19:22 +0530

    form data bind

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  userDetails = {
+    name: '',
+    post: ''
+  };
+  submitForm(form: any): void {
+    // if (form.valid) {
+    //   console.log('Form data:', this.userDetails);
+    // }lo
+
+    console.log(this.userDetails);
+
+  }
 
 }
